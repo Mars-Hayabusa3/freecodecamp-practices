7a50a0d58fdf738dde82ffc243068953bf0d37b7
Mars Chng <wayne.chuang.27@gmail.com>
2022-09-13 15:32:07 +0800

    練習

---
+++ object-keyvalue-pair.js
@@ -0,0 +1,21 @@
+// 2021年9月13日
+// freeCodeCamp > Basic JavaScript Course > Basic Data Structure 第13題
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  const banana = 'bananas';
+  foods[banana] = 13;
+  
+  const grapes = 'grapes';
+  foods[grapes] = 35;
+  
+  const erdbeer = 'strawberries';
+  foods[erdbeer] = 27;
+  // Only change code above this line
+  
+  console.log(foods);
