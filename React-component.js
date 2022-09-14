00bdac3aae49058226344d3616b1dcfdba9f246b
Mars Chng <wayne.chuang.27@gmail.com>
2022-09-14 13:53:13 +0800

    React

---
+++ React-component.js
@@ -0,0 +1,15 @@
+// 2021年9月14日
+// freeCodeCamp > Front End Development Libraries > React 第7題
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return(
+        <div>
+          <h1>Hello React!</h1>
+        </div>
+      );
+    }
+};
