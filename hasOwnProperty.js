674e87a83e5e93ffa1161e78ee8e7ab6f643a63e
Mars Chng <wayne.chuang.27@gmail.com>
2022-09-16 20:52:50 +0800

    finish basic data structures

---
+++ hasOwnProperty.js
@@ -0,0 +1,33 @@
+// 2021年9月16日
+// freeCodeCamp > Basic JavaScript Course > Basic Data Structure 第16題
+
+let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    if (
+      userObj.hasOwnProperty("Alan") &&
+      userObj.hasOwnProperty("Jeff") &&
+      userObj.hasOwnProperty("Sarah") &&
+      userObj.hasOwnProperty("Ryan")
+    ) {
+      return true;
+    }
+    return false;
+  }
---
+++ object-keys-array.js
@@ -0,0 +1,30 @@
+// 2021年9月16日
+// freeCodeCamp > Basic JavaScript Course > Basic Data Structure 第19題
+
+
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    // Only change code below this line
+    return Object.keys(obj);
+    // Only change code above this line
+  }
+  
+  console.log(getArrayOfUsers(users));
