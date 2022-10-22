7f7a4ee7eb8df7d0a482060b2717796417fddfd0
Mars Chng <wayne.chuang.27@gmail.com>
2022-10-22 12:09:00 +0800

    didMount

--- React-didMount.js
+++ React-didMount.js
@@ -1,3 +1,7 @@
+// Did Mount 和呼叫 API 相關
+// Most web developers, at some point, need to call an API endpoint to retrieve data. If you're working with React, it's important to know where to perform this action.
+// The best practice with React is to place API calls or any calls to your server in the lifecycle method componentDidMount(). This method is called after a component is mounted to the DOM. 
+
 class MyComponent extends React.Component {
     constructor(props) {
       super(props);
