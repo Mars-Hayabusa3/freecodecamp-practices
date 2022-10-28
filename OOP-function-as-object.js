75ad84c6580190c261cc96212ddbf1bc5f37f7c5
Mars Chng <wayne.chuang.27@gmail.com>
2022-10-28 17:36:12 +0800

    修錯

--- OOP-function-as-object.js
+++ OOP-function-as-object.js
@@ -1,7 +1,7 @@
 let dog = {
     name: "Spot",
     numLegs: 4,
-    sayLegs: () => {return "The god has " + dog.numLegs + " legs."}
+    sayLegs: () => {return "This dog has " + dog.numLegs + " legs."}
   };
   
   console.log(dog.sayLegs());
