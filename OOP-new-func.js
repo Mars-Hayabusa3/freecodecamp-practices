f2c698c66b21645a5d9a0701c231412dd4f72c33
Mars Chng <wayne.chuang.27@gmail.com>
2022-10-28 18:04:59 +0800

    OOP: construct functions by 'new'

---
+++ OOP-new-func.js
@@ -0,0 +1,9 @@
+// 用 new 創建一個新的同類型 function
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+  }
+  // Only change code below this line
+  const hound = new Dog();
