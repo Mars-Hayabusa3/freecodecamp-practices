8aa55938aafc0aa687cc620cab9a7136e5b24460
Mars Chng <wayne.chuang.27@gmail.com>
2022-11-23 01:44:02 +0800

    XHR: Post datas, Algorithm: 'Where do I belong'

---
+++ Algthm-Where-do-I-Belong.js
@@ -0,0 +1,21 @@
+function getIndexToIns(arr, num) {
+    arr.sort((a, b) => a - b);
+  
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] >= num) {
+        return i;
+      }
+    }
+  
+    return arr.length;
+  }
+  
+  console.log(getIndexToIns([40, 60], 50)); // 1
+  console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+  console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+  console.log(getIndexToIns([40, 60], 50)); // 1
+  console.log(getIndexToIns([3, 10, 5], 3)); // 0
+  console.log(getIndexToIns([5, 3, 20, 3], 5)); // 2
+  console.log(getIndexToIns([2, 20, 10], 19)); // 2
+  console.log(getIndexToIns([2, 5, 10], 15)); // 3
+  console.log(getIndexToIns([], 1)); // 0
