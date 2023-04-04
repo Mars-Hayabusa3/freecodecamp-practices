703512d61507c86bb4b01cdb4c00cfd8a3e13dac
Mars Chng <wayne.chuang.27@gmail.com>
2023-04-04 19:03:19 +0800

    React: Mapping practice

---
+++ React-Mapping-Key-attr.js
@@ -0,0 +1,21 @@
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map((v) => 
+      <li key={v}>{v}</li>); // Change this line
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
